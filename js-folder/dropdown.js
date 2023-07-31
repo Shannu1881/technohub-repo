b7ca50dcd85799ae787e85037c2d2bda13426279
Shannu1881 <109470585+Shannu1881@users.noreply.github.com>
2023-07-31 15:03:40 +0530

    course pages changes

---
+++ js-folder/dropdown.js
@@ -0,0 +1,7 @@
+function showDropdown() {
+    document.querySelector(".dropdown-menu").classList.add("show");
+}
+
+function hideDropdown() {
+    document.querySelector(".dropdown-menu").classList.remove("show");
+}
--- js-folder/galarySlide.js
+++ js-folder/galarySlide.js
@@ -1,17 +1,36 @@
+const slide = document.querySelector(".slides"),
+        firstImg = slide.querySelectorAll("img")[0];
+        arrowIcons = document.querySelectorAll(".wrapper i");
 
- const carousel = document.querySelector(".carousel");
+        let isDragStart = false, prevPageX, prevScrollLeft;
+        let firstImgWidth = firstImg.clientWidth + 14;
 
- let isDragStart = false;
+      arrowIcons.forEach(icon => {
+          icon.addEventListener("click", ()=>{
+            slide.scrollLeft += icon.id == "left" ? -firstImgWidth : firstImgWidth;
+          })
+      });
 
- const dragStart = () =>{
-    isDragStart = true;
- }
+        const dragStart = (e) => {
+            isDragStart = true;
+            prevPageX = e.pageX;
+            prevScrollLeft = slide.scrollLeft;
+        }
 
- const dragging = (e) =>{
-    if(!isDragStart) return
-    carousel.scrollLeft = e.pageX;
- }
+        const dragging = (e) => {
+            if (!isDragStart) return;
+            e.preventDefault();
+            slide.classList.add("dragging");
+            let positionDiff = e.pageX - prevPageX;
+            slide.scrollLeft = prevScrollLeft - positionDiff;
+        }
+
+        const dragStop = ()=>{
+            isDragStart = false;
+            slide.classList.remove("dragging");
+        }
 
 
- carousel.addEventListener("mousedown", dragStart);
- carousel.addEventListener("mousemove", dragging);
+        slide.addEventListener("mousedown", dragStart);
+        slide.addEventListener("mousemove", dragging);
+        slide.addEventListener("mouseup", dragStop);
